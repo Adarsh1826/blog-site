c5142f275279bd7d2b7903585c5f613ce4473120
adarsh singh <adsredmi182615@gmail.com>
2025-03-23 14:42:36 +0530

    removed exposed uri

--- backend/src/handlers/index.routes.ts
+++ backend/src/handlers/index.routes.ts
@@ -16,5 +16,3 @@ routes.post('/api/v1/blog/:id',(c)=>{
     return c.text('Hello')
 })
 export default routes;
-// DATABASE_URL="prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMzYzOTAzYmQtMjVjYy00Y2Y3LTk1ZDAtYWRlYTI1MzQ4MWM0IiwidGVuYW50X2lkIjoiYjRiZDY4OTcyNzAyZGI1ZDQ5M2M0NWZhN2ViZGQyMTMzMDdhOTZjOTFhOTkyYzYzNzA0MWM5Mzg0ZDFjMjNiOCIsImludGVybmFsX3NlY3JldCI6IjQwNTJlOTg4LWQyNTktNGY5My04NzIzLTIxZjYyZmJhMzcwYyJ9.EOuGhDJy9z3VOT473KBjGGL3NM0abLIW5Ue_ZWI4jxc"
-//postgresql://neondb_owner:npg_lxXOy1bD2UvH@ep-bitter-haze-a5qi596w-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require
