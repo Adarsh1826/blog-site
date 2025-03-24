db219dddc8d9487049ccb5b041ede3784b559dd8
adarsh singh <adsredmi182615@gmail.com>
2025-03-24 15:42:06 +0530

    hono routes

--- backend/src/handlers/index.routes.ts
+++ backend/src/handlers/index.routes.ts
@@ -1,7 +1,45 @@
 import { Hono } from "hono";
-const router = new Hono()
-router.post('/api/v1/sigup',(c)=>{
-    return c.text('Hello')
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import{decode,sign,verify} from "hono/jwt"
+const router = new Hono<{
+    Bindings:{
+        DATABASE_URL:string,
+        JWT_SEC:string
+    }
+}>()
+router.post('/api/v1/sigup',async (c)=>{
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+      }).$extends(withAccelerate());
+      const body = await c.req.json()
+      const{email,name,password} = body
+      const isOk = await prisma.user.findOne({
+        where:{
+            email:email
+        }
+      })
+      if(isOk){
+        if (isOk.length > 0) {
+            return c.json({
+                "msg": "Email already registered"
+            })
+        }
+      }
+      const user =await prisma.user.create({
+        data: {
+          email: email,
+          name:name,
+          password:password
+        },
+      })
+      const token = sign({id:user.id},c.env.JWT_SEC)
+      console.log(token);
+      return c.json({
+        "msg":"Registered Successfully",
+        jwt:token
+      })
+      
 })
 router.post('/api/v1/sigin',(c)=>{
     return c.text('Hello')
@@ -14,5 +52,8 @@ router.put('/api/v1/blog',(c)=>{
 })
 router.post('/api/v1/blog/:id',(c)=>{
     return c.text('Hello')
+})
+router.get('/',(c)=>{
+    return c.text('Hello ji')
 })
 export default router;
